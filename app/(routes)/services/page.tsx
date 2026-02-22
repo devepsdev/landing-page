35d0706c74b61ee59e7216217e92fd821429ec85
devepsdev <devepsdev@gmail.com>
2026-02-22 19:59:13 +0100

    front mobile

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -10,7 +10,7 @@ const ServicesPage = () => {
       <TransitionPage />
       <CircleImage />
       <AvatarServices />
-      <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
+      <div className="grid items-center justify-center max-w-5xl gap-6 mx-auto md:grid-cols-2 md:h-screen pt-32 pb-32 md:pt-0 md:pb-0 px-4 md:px-0">
         <div className="max-w-[450px]">
           <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">
             Mis <span className="font-bold text-secondary"> servicios.</span>
