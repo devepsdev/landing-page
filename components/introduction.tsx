7334057b526f09bce9c1a5d3fdea96876a0f0d5c
devepsdev <devepsdev@gmail.com>
2026-02-23 08:24:56 +0100

    avatar

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -5,14 +5,13 @@ import { TypeAnimation } from "react-type-animation";
 const Introduction = () => {
   return (
     <div className="z-20 w-full bg-darkBg/60">
-      <div className="z-20 grid items-center h-full p-6 pt-24 pb-48 md:py-0 md:grid-cols-2">
+      <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2">
         <Image
           src="/home-4.png"
           priority
           width="800"
           height="800"
           alt="Avatar"
-          className="w-48 h-auto mx-auto md:w-auto md:mx-0"
         />
         <div className="flex flex-col justify-center max-w-md">
           <h1 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">
